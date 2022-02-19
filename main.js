1587583c5c9c6f3f80963c55fb93f593b4aeb77a
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-02-19 17:40:00 +0200

    fix

---
+++ main.js
@@ -0,0 +1,18 @@
+var btnLang = document.getElementById('lang');
+
+var isFocused = (document.activeElement === btnLang);
+
+
+btnLang.onclick = function closeLang() {
+
+    if (btnLang = true) {
+
+        // btnLang.onclick.document.activeElement;
+        console.log('zzzz');
+
+    }
+}
+
+
+
+
