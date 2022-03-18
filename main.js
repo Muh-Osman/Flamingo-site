ec60470bf7cff492a8dab75fe729a2dcea65141f
Muhammed Osman <91621437+Muh-Osman@users.noreply.github.com>
2022-03-18 11:23:04 +0200

    fix

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ if (darkMode === 'enabled') {
 // When someone clicks the button
 darkModeToggle.addEventListener('change', () => {
 
-    navigator.vibrate(50); // vibrate for 50ms
+    navigator.vibrate(100); // vibrate for 100ms
 
     // set checked input in LocalStorage if User click on it
     localStorage.setItem('dark-mode-toggle', darkModeToggle.checked);
